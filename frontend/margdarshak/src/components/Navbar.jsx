bd13b9db786a38fbb8be035a384760fe0a79fe68
Your Name <you@example.com>
2025-04-12 22:23:25 +0530

    Updates

--- frontend/margdarshak/src/components/Navbar.jsx
+++ frontend/margdarshak/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ function Navbar() {
     <nav className="bg-white  ">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Left - Logo */}
-        <div className="text-2xl font-bold text-neutral-900 tracking-tighter">Career-Advisor</div>
+        <a href="/">
+          <div className="text-2xl font-bold text-neutral-900 tracking-tighter">Career-Advisor</div>
+          </a>
 
         {/* Center - Links */}
         <ul className="hidden md:flex  gap-8 text-neutral-700 text-[16px] font-medium">
@@ -30,18 +32,11 @@ function Navbar() {
         {/* Right - Sign In Button */}
         <div>
           <div
-<<<<<<< HEAD
-            className="flex bg-neutral-900 tracking-tighter text-white text-[18px] px-4 py-2 rounded-sm hover:bg-neutral-700 transition-all duration-200 gap-1.5 items-center"
-          >
-            Sign Up
-            <GoArrowRight size={22} />
-=======
             className="flex flex-row  bg-neutral-900 tracking-tighter text-white text-[18px] px-4 py-3 rounded-sm hover:bg-neutral-700 transition-all duration-200 gap-1.5 items-center"
           ><Link to="/sign-up" className='flex gap-1.5 items-center'>
 
               SignUp <GoArrowRight size={22} />
             </Link>
->>>>>>> 40ea431c7fb8548881b8021b8dfa7b895c989b7a
           </div>
         </div>
       </div>
