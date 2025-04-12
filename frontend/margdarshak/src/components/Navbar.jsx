99ec1c6f0e34e9f5e919110bb907ac2161b74b83
Karmpatel18 <karmpatel1203@gmail.com>
2025-04-12 11:20:41 +0530

    navbar change

--- frontend/margdarshak/src/components/Navbar.jsx
+++ frontend/margdarshak/src/components/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import { GoArrowRight } from "react-icons/go";
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white  ">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Left - Logo */}
-        <div className="text-2xl font-bold text-blue-600">LOGO</div>
+        <div className="text-2xl font-bold text-neutral-900 tracking-tighter">Career-Advisor</div>
 
         {/* Center - Links */}
-        <ul className="hidden md:flex gap-8 text-gray-700 text-lg font-medium">
-          <li><a href="/assessment" className="hover:text-blue-600">Assessment</a></li>
-          <li><a href="/resume" className="hover:text-blue-600">Resume Tips</a></li>
-          <li><a href="/roadmaps" className="hover:text-blue-600">Career Roadmaps</a></li>
-          <li><a href="/book-slot" className="hover:text-blue-600">Book a Slot</a></li>
+        <ul className="hidden md:flex  gap-8 text-neutral-700 text-[16px] font-medium">
+          <li><a href="/assessment" className="hover:text-neutral-400 tracking-tight transition-all duration-300">Assessment</a></li>
+          <li><a href="/resume" className="tracking-tight hover:text-neutral-400 transition-all duration-300">Resume Tips</a></li>
+          <li><a href="/roadmaps" className="tracking-tight hover:text-neutral-400 transition-all duration-300">Career Roadmaps</a></li>
+          <li><a href="/book-slot" className="tracking-tight hover:text-neutral-400 transition-all duration-300">Book a Slot</a></li>
         </ul>
 
         {/* Right - Sign In Button */}
         <div>
-          <button 
-            className="bg-blue-600 text-white px-4 py-1.5 rounded-md hover:bg-blue-700 transition"
+          <div 
+            className="flex bg-neutral-900 tracking-tighter text-white text-[18px] px-4 py-3 rounded-sm hover:bg-neutral-700 transition-all duration-200 gap-1.5"
           >
-            Sign In
-          </button>
+            Sign Up
+            <GoArrowRight size={22}/>
+          </div>
         </div>
       </div>
     </nav>
